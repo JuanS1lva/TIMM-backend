1eb66c9bcdb7fe141a2beb7169be2d8b7aa58aff
JuanS1lva <juan.silva.vane@gmail.com>
2021-05-17 22:33:42 -0500

    Se creo CRUD proyectos

--- functions/index.js
+++ functions/index.js
@@ -32,3 +32,84 @@ exports.addMessage = functions.https.onCall(async (data) => {
     original,
   }
 });
+
+exports.createProyect = functions.https.onCall(async (data) => {
+  // Grab the text parameter.
+  console.log(data);
+  const infoProyecto = {
+    nombre: data.nombre,
+    responsable: data.responsable
+  }
+
+  // Push the new message into Firestore using the Firebase Admin SDK.
+  const proyectoNuevo = await admin.firestore().collection('proyectos').add({...infoProyecto});
+  // Send back a message that we've successfully written the message
+  return {
+    code: 'ok',
+    message: `Proyecto nuevo con el ID: ${proyectoNuevo.id} agregado.`,
+    infoProyecto,
+  }
+});
+
+
+exports.editProyect = functions.https.onCall(async (data) => {
+  // Grab the text parameter.
+  console.log(data);
+  const infoProyecto = {
+    nombre: data.nombre,
+    responsable: data.responsable
+  }
+  const targetDocument = data.proyecto
+
+  // Push the new message into Firestore using the Firebase Admin SDK.
+  const proyectoUpdate = await admin.firestore()
+    .collection('proyectos')
+    .doc(targetDocument)
+    .update({...infoProyecto});
+  // Send back a message that we've successfully written the message
+  return {
+    code: 'ok',
+    message: `Proyecto actualizado con el ID: ${targetDocument} agregado.`,
+    infoProyecto,
+  }
+});
+
+exports.deleteProyect = functions.https.onCall(async (data) => {
+  // Grab the text parameter.
+  console.log(data);
+
+  const targetDocument = data.proyecto
+
+  // Push the new message into Firestore using the Firebase Admin SDK.
+  const proyectoUpdate = await admin.firestore()
+    .collection('proyectos')
+    .doc(targetDocument)
+    .delete();
+  // Send back a message that we've successfully written the message
+  return {
+    code: 'ok',
+    message: `Proyecto borrado correctamente.`,
+  }
+});
+
+exports.getProyect = functions.https.onCall(async (data) => {
+  // Grab the text parameter.
+  console.log(data);
+
+  const targetDocument = data.proyecto
+
+  // Push the new message into Firestore using the Firebase Admin SDK.
+  let snapshot = await admin.firestore()
+    .collection('proyectos')
+    .doc(targetDocument)
+    .get();
+
+  snapshot = snapshot.data()
+
+  return {
+    code: 'ok',
+    message: `Proyecto encontrado.`,
+    snapshot
+  }
+});
+
